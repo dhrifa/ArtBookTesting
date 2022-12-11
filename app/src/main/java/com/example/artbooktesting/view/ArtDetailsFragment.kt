8c22866fac26766e14f8588223c2f1a718108812
dhrifa <dhrifa.kherroubi@mobileconsultingsolutions.us>
2022-12-11 14:29:30 -0500

    implement navigations code

---
+++ app/src/main/java/com/example/artbooktesting/view/ArtDetailsFragment.kt
@@ -0,0 +1,36 @@
+package com.example.artbooktesting.view
+
+import android.os.Bundle
+import android.view.View
+import androidx.activity.OnBackPressedCallback
+import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import com.example.artbooktesting.R
+import com.example.artbooktesting.databinding.FragmentArtDetailsBinding
+
+class ArtDetailsFragment : Fragment(R.layout.fragment_art_details) {
+
+    private var fragmentBinding: FragmentArtDetailsBinding? = null
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val binding = FragmentArtDetailsBinding.bind(view)
+        fragmentBinding = binding
+        binding.artImageView.setOnClickListener {
+            findNavController().navigate(ArtDetailsFragmentDirections.actionArtDetailsFragmentToImageApiFragment())
+        }
+
+        val callBack = object : OnBackPressedCallback(true) {
+            override fun handleOnBackPressed() {
+                findNavController().popBackStack()
+            }
+        }
+        requireActivity().onBackPressedDispatcher.addCallback(callBack)
+    }
+
+    override fun onDestroy() {
+        fragmentBinding = null
+        super.onDestroy()
+    }
+}
