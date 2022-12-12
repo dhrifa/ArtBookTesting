0d8404e7bf84fafb66737b25b0635f7f5f965581
dhrifa <dhrifa.kherroubi@mobileconsultingsolutions.us>
2022-12-12 09:22:00 -0500

    implement retrofit api & util

---
+++ app/src/main/java/com/example/artbooktesting/util/Util.kt
@@ -0,0 +1,6 @@
+package com.example.artbooktesting.util
+
+object Util {
+    const val API_KEY = "32003760-057c2cf4486fc0c1099fc0b82"
+    const val BASE_URL = "https://pixabay.com"///api/docs/"
+}
