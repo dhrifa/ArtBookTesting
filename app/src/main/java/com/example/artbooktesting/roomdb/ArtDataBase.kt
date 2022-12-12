f08295185ea663a98b92ccc2dfeedda892c40619
dhrifa <dhrifa.kherroubi@mobileconsultingsolutions.us>
2022-12-12 11:35:07 -0500

    fix database class ArtDataBase: RoomDatabase()

--- app/src/main/java/com/example/artbooktesting/roomdb/ArtDataBase.kt
+++ app/src/main/java/com/example/artbooktesting/roomdb/ArtDataBase.kt
@@ -2,8 +2,9 @@ package com.example.artbooktesting.roomdb
 
 import androidx.room.Database
 import androidx.room.Entity
+import androidx.room.RoomDatabase
 
 @Database(entities = [Art::class], version = 1)
-abstract class ArtDataBase {
+abstract class ArtDataBase: RoomDatabase() {
 abstract fun artDao(): ArtDao
 }
