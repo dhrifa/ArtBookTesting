7342bc7f41b019cb37537173ad1487225276598f
dhrifa <dhrifa.kherroubi@mobileconsultingsolutions.us>
2022-12-14 12:15:29 -0500

    implement image recycler view

---
+++ app/src/main/java/com/example/artbooktesting/adaper/ImageRecyclerAdapter.kt
@@ -0,0 +1,66 @@
+package com.example.artbooktesting.adaper
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.View.OnClickListener
+import android.view.ViewGroup
+import android.widget.ImageView
+import androidx.appcompat.view.menu.MenuView.ItemView
+import androidx.recyclerview.widget.AsyncListDiffer
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.RequestManager
+import com.example.artbooktesting.R
+import com.example.artbooktesting.model.ImageResponse
+import com.example.artbooktesting.model.ImageResult
+import javax.inject.Inject
+
+class ImageRecyclerAdapter @Inject constructor(
+    val glide: RequestManager
+) : RecyclerView.Adapter<ImageRecyclerAdapter.ImageViewHolder>() {
+
+    class ImageViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView)
+
+    private var onItemClickListener: ((String) -> Unit)? = null
+
+    private val diffUtil = object : DiffUtil.ItemCallback<String>() {
+        override fun areItemsTheSame(oldItem: String, newItem: String): Boolean {
+            return oldItem == newItem
+        }
+
+        override fun areContentsTheSame(oldItem: String, newItem: String): Boolean {
+            return oldItem == newItem
+        }
+    }
+
+    private val recyclerListDiffer = AsyncListDiffer(this, diffUtil)
+    var images: List<String>
+        get() = recyclerListDiffer.currentList
+        set(value) = recyclerListDiffer.submitList(value)
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ImageViewHolder {
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.image_row, parent, false)
+        return ImageViewHolder((view))
+    }
+
+    fun setOnItemClickListener(listener: (String) -> Unit) {
+        onItemClickListener = listener
+    }
+
+    override fun onBindViewHolder(holder: ImageViewHolder, position: Int) {
+        val imageView = holder.itemView.findViewById<ImageView>(R.id.singleArtImageView)
+        val imageUrl = images[position]
+        holder.apply {
+            glide.load(imageUrl).into(imageView)
+            setOnItemClickListener {
+                onItemClickListener?.let {
+                    it(imageUrl)
+                }
+            }
+        }
+    }
+
+    override fun getItemCount(): Int {
+        return images.size
+    }
+}
