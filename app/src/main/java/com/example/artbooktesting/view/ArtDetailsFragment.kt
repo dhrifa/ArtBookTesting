2b3f875f5d2c10280243abff1b8e438bf74965cf
dhrifa <dhrifa.kherroubi@mobileconsultingsolutions.us>
2022-12-14 14:26:16 -0500

    insert image

--- app/src/main/java/com/example/artbooktesting/view/ArtDetailsFragment.kt
+++ app/src/main/java/com/example/artbooktesting/view/ArtDetailsFragment.kt
@@ -2,12 +2,17 @@ package com.example.artbooktesting.view
 
 import android.os.Bundle
 import android.view.View
+import android.widget.Toast
 import androidx.activity.OnBackPressedCallback
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.get
 import androidx.navigation.fragment.findNavController
 import com.bumptech.glide.RequestManager
 import com.example.artbooktesting.R
 import com.example.artbooktesting.databinding.FragmentArtDetailsBinding
+import com.example.artbooktesting.util.Status
+import com.example.artbooktesting.viewmodel.ArtViewModel
 import javax.inject.Inject
 
 class ArtDetailsFragment @Inject constructor(
@@ -15,12 +20,18 @@ class ArtDetailsFragment @Inject constructor(
 : Fragment(R.layout.fragment_art_details) {
 
     private var fragmentBinding: FragmentArtDetailsBinding? = null
+    lateinit var viewModel: ArtViewModel
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        viewModel = ViewModelProvider(requireActivity()).get(ArtViewModel::class.java)
+
         val binding = FragmentArtDetailsBinding.bind(view)
         fragmentBinding = binding
+
+        subscribeToObservers()
+
         binding.artImageView.setOnClickListener {
             findNavController().navigate(ArtDetailsFragmentDirections.actionArtDetailsFragmentToImageApiFragment())
         }
@@ -31,6 +42,32 @@ class ArtDetailsFragment @Inject constructor(
             }
         }
         requireActivity().onBackPressedDispatcher.addCallback(callBack)
+
+        binding.saveButton.setOnClickListener {
+            viewModel.makeArt(binding.nameText.text.toString(), binding.artistText.text.toString(), binding.yearText.text.toString())
+        }
+    }
+
+    private fun subscribeToObservers(){
+        viewModel.selectedImageUrl.observe(viewLifecycleOwner){ url ->
+            fragmentBinding?.let {
+                glide.load(url).into(it.artImageView)
+            }
+        }
+
+        viewModel.insertArtMessage.observe(viewLifecycleOwner){
+            when(it.status){
+                Status.SUCCESS ->{
+                    Toast.makeText(requireContext(), "Success", Toast.LENGTH_LONG).show()
+                    findNavController().popBackStack()
+                    viewModel.resetInsertArtMsg()
+                }
+                Status.LOADING ->{}
+                Status.ERROR ->{
+                    Toast.makeText(requireContext(), it.message ?: "Error", Toast.LENGTH_LONG).show()
+                }
+            }
+        }
     }
 
     override fun onDestroy() {
