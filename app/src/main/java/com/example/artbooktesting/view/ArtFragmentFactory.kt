237b7adf98b1a99e9e069231a37caec25c2b2c07
dhrifa <dhrifa.kherroubi@mobileconsultingsolutions.us>
2022-12-14 13:49:41 -0500

    add dependencies to constructor + instantiate missing frags

--- app/src/main/java/com/example/artbooktesting/view/ArtFragmentFactory.kt
+++ app/src/main/java/com/example/artbooktesting/view/ArtFragmentFactory.kt
@@ -3,14 +3,20 @@ package com.example.artbooktesting.view
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentFactory
 import com.bumptech.glide.RequestManager
+import com.example.artbooktesting.adaper.ArtRecyclerAdapter
+import com.example.artbooktesting.adaper.ImageRecyclerAdapter
 import javax.inject.Inject
 
 class ArtFragmentFactory @Inject constructor(
-    private val glide : RequestManager
+    private val artRecyclerAdapter: ArtRecyclerAdapter,
+    private val glide : RequestManager,
+    private val imageRecyclerAdapter: ImageRecyclerAdapter
 ) : FragmentFactory() {
     override fun instantiate(classLoader: ClassLoader, className: String): Fragment {
         return when(className){
+            ArtFragment::class.java.name -> ArtFragment(artRecyclerAdapter)
             ArtDetailsFragment::class.java.name -> ArtDetailsFragment(glide)
+            ImageApiFragment::class.java.name -> ImageApiFragment(imageRecyclerAdapter)
             else -> super.instantiate(classLoader, className)}
     }
 }
