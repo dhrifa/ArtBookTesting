1118a09cc6875326113c2855ab2437bda607bea0
dhrifa <dhrifa.kherroubi@mobileconsultingsolutions.us>
2022-12-15 21:03:46 -0500

    hilt testing: implement TestAppModule

---
+++ app/src/androidTest/java/com/example/artbooktesting/dependencyinjection/TestAppModule.kt
@@ -0,0 +1,24 @@
+package com.example.artbooktesting.dependencyinjection
+
+import android.content.Context
+import androidx.room.Room
+import com.example.artbooktesting.roomdb.ArtDataBase
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Named
+
+@Module
+@InstallIn(SingletonComponent::class)
+object TestAppModule {
+
+    @Provides
+    @Named("testDatabase")
+    fun injectInMemoryRoom(@ApplicationContext context: Context) =
+        Room.inMemoryDatabaseBuilder(context, ArtDataBase::class.java)
+            .allowMainThreadQueries()
+            .build()
+
+}
